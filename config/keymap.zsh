f936fc54b5b4bb5169d440c342e72af19d0df1ec
не круто <zoonyanapat@gmail.com>
2023-12-04 01:41:44 +0700

    add more config file

---
+++ config/keymap.zsh
@@ -0,0 +1,8 @@
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+
+alias open="xdg-open"
+alias ls="lsd"
+alias fetch="fastfetch"
+alias c="clear"
+alias dup="sudo dnf update -y && sudo dnf upgrade -y"
---
+++ config/pfetch_config.zsh
@@ -0,0 +1,2 @@
+export PF_INFO="ascii title os uptime pkgs memory editor palette"
+pfetch
